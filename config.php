7cc112cb613675bce6d716e1839efe37f361ee48
Julian Thompson <julian@atomicant.co.uk>
2013-12-15 18:29:29 +0000

    added new devices

---
+++ config.php
@@ -0,0 +1,64 @@
+<?php
+
+$devices = array(
+	'a1' => new JSObject(array(
+		'title' => "Standard Lamp",
+		'location' => "Lounge",
+		'code' => 1,
+		'housecode' => 'a',
+		'description' => "",
+		'dimmable' => TRUE,
+		'currentlevel' => 100,
+		'lastupdate' => 0
+	)), 
+		'a2' => new JSObject(array(
+		'title' => "Media System",
+		'location' => "Lounge",
+		'code' => 2,
+		'housecode' => 'a',
+		'description' => "",
+		'dimmable' => FALSE,
+		'currentlevel' => 100,
+		'lastupdate' => 0
+	)), 
+		'a3' => new JSObject(array(
+		'title' => "Ikea Lamp",
+		'location' => "Dining room",
+		'code' => 3,
+		'housecode' => 'a',
+		'description' => "",
+		'dimmable' => TRUE,
+		'currentlevel' => 100,
+		'lastupdate' => 0
+	)), 
+		'b1' => new JSObject(array(
+		'title' => "Bedside Lamp",
+		'code' => 1,
+		'housecode' => 'b',
+		'location' => "Bedroom 1",
+		'description' => "",
+		'dimmable' => TRUE,
+		'currentlevel' => 100,
+		'lastupdate' => 0 
+	)),
+		'b2' => new JSObject(array(
+		'title' => "Screen",
+		'location' => "Bedroom 1",
+		'code' => 2,
+		'housecode' => 'b',
+		'description' => "",
+		'dimmable' => FALSE,
+		'currentlevel' => 100,
+		'lastupdate' => 0
+	)),
+		'b3' => new JSObject(array(
+		'title' => "Bedside lamp",
+		'location' => "Bedroom 2",
+		'code' => 3,
+		'housecode' => 'b',
+		'description' => "",
+		'dimmable' => FALSE,
+		'currentlevel' => 100,
+		'lastupdate' => 0
+	)) 
+);
